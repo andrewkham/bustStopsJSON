692d08489f849d205c3f8b313e4c76382abc081b
Andrew Ham <hampossible@gmail.com>
2015-06-28 18:28:36 -0700

    created route 704 stops json with api_id to string

--- db/migrate/20150629012345_create_stops.rb
+++ db/migrate/20150629012345_create_stops.rb
@@ -4,7 +4,7 @@ class CreateStops < ActiveRecord::Migration
       t.float :latitude
       t.float :longitude
       t.string :display_name
-      t.integer :api_id
+      t.string :api_id
 
       t.timestamps null: false
     end
